4e6173b09fff693f350277ca197bfaef706dd88d
Phil Nichols <philipdnichols@gmail.com>
2014-08-25 17:36:13 -0400

    Added modal background.

--- PNRadialMenu/PNViewController.m
+++ PNRadialMenu/PNViewController.m
@@ -15,6 +15,7 @@
 @interface PNViewController ()
 
 @property (strong, nonatomic) PNRadialMenuMainButton *mainButton;
+@property (strong, nonatomic) UIView *modalView;
 
 @end
 
@@ -24,26 +25,57 @@
 {
     [super viewDidLoad];
     
+    self.modalView = [[UIView alloc] initWithFrame:self.view.frame];
+    self.modalView.hidden = YES;
+    
+    [self.view addSubview:self.modalView];
+    
+    UITapGestureRecognizer *modalViewTapGestureRecogizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(modalViewTapped)];
+    [self.modalView addGestureRecognizer:modalViewTapGestureRecogizer];
+    
     self.mainButton = [[PNRadialMenuMainButton alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
     self.mainButton.center = self.view.center;
     
     [self.view addSubview:self.mainButton];
     
-    UITapGestureRecognizer *mainButtonTapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(mainButtonTapped:)];
+    UITapGestureRecognizer *mainButtonTapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(mainButtonTapped)];
     [self.mainButton addGestureRecognizer:mainButtonTapGestureRecognizer];
 }
 
-- (void)mainButtonTapped:(UITapGestureRecognizer *)tapGestureRecognizer
+#pragma mark - IBActions
+
+- (IBAction)mainButtonTapped
 {
-    [UIView animateWithDuration:0.5
+    [self animateTogglePresentAndDismissRadialMenu];
+}
+
+- (IBAction)modalViewTapped
+{
+    [self animateTogglePresentAndDismissRadialMenu];
+}
+
+#pragma mark - Animations
+
+- (void)animateTogglePresentAndDismissRadialMenu
+{
+    [UIView animateWithDuration:0.35
                      animations:^{
                          if (self.mainButton.isMenuDisplayed) {
+                             self.modalView.backgroundColor = [UIColor clearColor];
+                             
                              self.mainButton.transform = CGAffineTransformIdentity;
                          } else {
+                             self.modalView.hidden = NO;
+                             self.modalView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.75];
+                             
                              self.mainButton.transform = CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(-45));
                          }
                      } completion:^(BOOL finished) {
                          self.mainButton.isMenuDisplayed = !self.mainButton.isMenuDisplayed;
+                         
+                         if (!self.mainButton.isMenuDisplayed) {
+                             self.modalView.hidden = YES;
+                         }
                      }];
 }
 
